30d12460acf3a4acdc2a288a1f61ee756d2a7202
Mansour <mansour@foobar.agency>
2025-06-28 20:18:11 +0200

    Initial Commit

---
+++ PodcastDownloader/PodcastDownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodcastDownloaderApp.swift
+//  PodcastDownloader
+//
+//  Created by Mansour Kalagar on 28.06.25.
+//
+
+import SwiftUI
+
+@main
+struct PodcastDownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PodcastDownloaderTests/PodcastDownloaderTests.swift
@@ -0,0 +1,17 @@
+//
+//  PodcastDownloaderTests.swift
+//  PodcastDownloaderTests
+//
+//  Created by Mansour Kalagar on 28.06.25.
+//
+
+import Testing
+@testable import PodcastDownloader
+
+struct PodcastDownloaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
